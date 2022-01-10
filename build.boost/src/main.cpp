44e01e5db25545dd82fe29ef7806e0720d4d0c93
scratch-visitor <scratch.visitor@gmail.com>
2022-01-10 19:00:53 +0900

    Update: tiny update  - use cmake file glob.  - add showprompt, string right trim.

--- build.boost/src/main.cpp
+++ build.boost/src/main.cpp
@@ -2,13 +2,17 @@
 #include <string>
 #include <boost/regex.hpp> //! regex need library, is not header file only.
 
+bool showprompt();
+std::string trim(std::string const&);
+
 int main()
 {
   std::string line;
   boost::regex pat("^hello (.*)");
 
-  while (std::getline(std::cin, line))
+  while (showprompt() && std::getline(std::cin, line))
   {
+    line = trim(line);
     if (line == "exit") break;
 
     boost::smatch matches;
@@ -19,4 +23,15 @@ int main()
   }
 
   return 0;
+}
+
+bool showprompt()
+{
+  std::cout << "> ";
+  return true;
+}
+std::string trim(std::string const& s)
+{
+  std::string tmp = s.substr(0, s.find_last_not_of(" \t") + 1);
+  return tmp;
 }
